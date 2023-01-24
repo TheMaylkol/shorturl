606d4d6d27da31572b1b1aba0df4d14926f71380
TheMaylkol <42708107+TheMaylkol@users.noreply.github.com>
2023-01-24 20:03:19 -0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer () {
     <div className="border-t border-neutral-800">
       <Container className="h-16 flex items-center justify-center">
         <Text priority="low" className="flex items-center text-sm">
-          Made with <Heart className="mx-2" /> and a lot of <Coffee className="mx-2" /> by <a href="https://www.reburn.dev" className="mx-2 font-bold text-white" rel="noopener noreferrer" target="_blank">reburn.dev</a>
+          Desarrollado con el <Heart className="mx-2" /> Y un poco de <Coffee className="mx-2" /> por: <a href="https://www.reburn.dev" className="mx-2 font-bold text-white" rel="noopener noreferrer" target="_blank">reburn.dev</a>
         </Text>
       </Container>
     </div>
