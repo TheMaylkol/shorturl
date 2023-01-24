8740180d622e908cfed99615ccd2e91fdd9e221d
TheMaylkol <42708107+TheMaylkol@users.noreply.github.com>
2023-01-24 19:59:56 -0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,8 +8,8 @@ export function Navbar () {
     <div className="z-10 sticky top-0 backdrop-blur-sm bg-neutral-900/90 border-b border-neutral-800">
       <Container className="h-16 flex items-center">
         <Text priority="medium">
-          <a href="https://www.reburn.dev" target="_blank" rel="noopener noreferrer">
-            Reburn Shortener
+          <a href="https://www.radiofavorita.cl" target="_blank" rel="noopener noreferrer">
+            RADIO FAVORITA FM
           </a>
         </Text>
         <div className="flex-1"></div>
