d82b0f75af06c79cea230c2d2166a54a4636a8a0
TheMaylkol <42708107+TheMaylkol@users.noreply.github.com>
2023-01-24 20:05:07 -0300

    Update ShortUrlList.tsx

--- components/ShortUrlList.tsx
+++ components/ShortUrlList.tsx
@@ -12,16 +12,16 @@ export function ShortUrlList () {
     <div>
       <div className="border-y border-neutral-800 h-16 justify-center flex items-center">
         <Text className="text-sm" priority="low">
-          Shorten links
+          Acordator de URL
         </Text>
       </div>
       <div className="py-20">
         <Container size="smaller">
           <h2 className="text-3xl text-center font-bold mb-2">
-            All your links
+            Todos tus enlances
           </h2>
           <Text priority="medium" className="mb-10 text-center text-sm leading-6">
-            Remember that all links are stored locally, so be very careful.
+            Recuerda que todos los enlaces se almacenan localmente, así que ten mucho cuidado.
           </Text>
         </Container>
         <Container size="small">
